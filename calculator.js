e16d99386ce820eefe3e92d651e2676d26a6ae1b
next1code <next1code@yahoo.com>
2025-02-26 11:39:30 +0430

    style the some buttons and background

--- calculator.js
+++ calculator.js
@@ -11,6 +11,18 @@ for (let i = 0; i < buttonValues.length; i++) {
   let button = document.createElement('button');
   button.innerText = value;
 
+  // styling button colors
+  if (value === '0') {
+    button.style.width = "200px";
+    button.style.gridColumn = "span 2";
+  }
+  if (rightSymbols.includes(value)) {
+    button.style.backgroundColor = "#ff9500";
+  } else if (topSymbols.includes(value)) {
+    button.style.backgroundColor = "#D4D4D2";
+    button.style.color = "#1c1c1c";
+  }
+
   // add buttons to calculator
   document.getElementById('buttons').appendChild(button);
 }
